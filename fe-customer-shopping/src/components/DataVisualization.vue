7b8b40ae9a8de3bcf4bcaf156c47a2bbf09b0cd9
Leonardho Rante S <leonardho81@gmail.com>
2025-02-19 04:41:16 +0700

    [FE] Default Component and Pie Chart

---
+++ fe-customer-shopping/src/components/DataVisualization.vue
@@ -0,0 +1,27 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <O_PieChartComponent 
+      :series="[44, 55]" 
+      :labels="['Team A', 'Team B']" 
+      second_title="Gender Comparison" 
+      content="This compare total customer purchase by its gender"
+    />
+  </WelcomeItem>
+</template>
+
+<script>
+  import O_PieChartComponent from "@/components/organisms/O_PieChartComponent.vue"
+  export default {
+    components: {
+      O_PieChartComponent
+    }
+  }
+</script>
